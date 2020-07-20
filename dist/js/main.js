ea95e6d8d4de9f2113116fe95cadd4d16158dd76
Niraj Chowdhary <chowdharyniraj97@gmail.com>
2020-07-20 05:28:07 -0400

    added menu effects

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,10 @@
-//Select Dom elements
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-items");
+const navItems = document.querySelectorAll(".nav-item");
 
-//Set initial stae of menu
-
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
@@ -17,16 +15,18 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
+    navItems.forEach((item) => item.classList.add("show"));
 
-    navItems.forEach((items) => items.classList.add("show"));
+    // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
+    navItems.forEach((item) => item.classList.remove("show"));
 
-    navItems.forEach((items) => items.classList.remove("show"));
+    // Set Menu State
     showMenu = false;
   }
 }
